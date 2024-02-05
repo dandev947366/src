afa84084d83ce32c41fafdfb1bb491a9e1e8ab4e
dandev947366 <dan.dev947366@gmail.com>
2024-02-05 18:06:39 +0200

    task7

---
+++ DL_tasks/ex7.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+            rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;1,400;1,600&family=Montserrat:ital,wght@0,300;0,400;0,700;1,500&display=swap" rel="stylesheet">
+        <title>Restaurant</title>
+    </head>
+    <body>
+    <?php include ('header.php'); ?>
+    
+<h2 class="mt-4">Input Your Information Below:</h2>
+<form name="form1 class="md-4" method="post" action="">
+    <div class="form-group">
+        <div class="row">
+            <div class="col">
+                <label for="fname">First Name:</label>
+                <input type="text" class="form-control" id="fname" placeholder="Enter your first name" name="fname" required>
+            </div>
+            <div class="col">
+                <label for="lname">Last Name:</label>
+                <input type="text" class="form-control" id="lname" placeholder="Enter your last name" name="lname" required>
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="row">
+            <div class="col">
+                <label for="city">City:</label>
+                <input type="text" class="form-control" id="city" placeholder="Enter your city" name="city" required>
+            </div>
+            <div class="col">
+                <label for="groupid">Group ID:</label>
+                <select class="form-control" id="groupid" name="groupid">
+                    <option value="BBCAP19">BBCAP19</option>
+                    <option value="BBCAP20">BBCAP20</option>
+                    <option value="BBCAP21">BBCAP21</option>
+                    <option value="BBCAP22">BBCAP22</option>
+                    <option value="Others">Others</option>
+                </select>
+            </div>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+</form>
+<?php
+// Check if the 'submit' button in the form was clicked
+if (isset($_POST['submit'])) {
+    // Retrieve data from the form and store it in variables
+    $fname = $_POST['fname'];     // First name
+    $lname = $_POST['lname'];     // Last name
+    $city = $_POST['city'];       // City
+    $groupid = $_POST['groupid']; // Group ID
+
+    // Include the database connection file
+    include 'dbshell.php';
+
+    // Define an SQL query to insert data into the 'studentsinfo' table
+    $sql = "INSERT INTO studentsinfo (first_name, last_name, city, groupId)
+            VALUES ('$fname', '$lname', '$city', '$groupid')";
+
+    // Execute the SQL query using the database connection
+    if ($conn->query($sql) === TRUE) {
+        // If the query was successful, display a success message
+        echo "New record added";
+    } else {
+        // If there was an error in the query, display an error message
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close the database connection
+    $conn->close();
+}
+?>
+<?php include 'footer.php'; ?>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"></script>
+    </body>
+</html>
+
--- DL_tasks/header.php
+++ DL_tasks/header.php
@@ -18,6 +18,9 @@
                         <li>
                             <a class="nav-link" href="ex4.php">Ex4</a>
                         </li>
+                        <li>
+                            <a class="nav-link" href="ex7.php">Ex7</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
